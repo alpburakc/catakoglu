d91f621aa60329e614f319a6f8e12db9fc6fe80c
Alp Burak Catakoglu <alpburak@hotmail.com>
2023-04-11 12:39:38 +0300

    delete tina

--- tina/__generated__/client.ts
+++
@@ -1,5 +0,0 @@
-import { createClient } from "tinacms/dist/client";
-import { queries } from "./types";
-export const client = createClient({ url: 'https://content.tinajs.io/1.4/content/e5717c0d-03d6-48e9-aff6-88540ee1c37b/github/main', token: '7804ee905e780392597aefa9b027572ed95052e3', queries });
-export default client;
-  
